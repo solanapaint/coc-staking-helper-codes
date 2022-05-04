d55db0dba40f875600bbf387c4d1e21d8e7f7a99
Solana Paint <solanapaint@gmail.com>
2022-05-04 11:54:41 +0530

    init: add code for check_staked_nfts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# coc-staking-helper-codes
+# How to use
+
+```sh
+# Install dependencies
+yarn
+# Run command to get list of staked NFTs of a given user
+ts-node cli.ts check_staked_nfts -w <user_wallet>
+# For example
+ts-node cli.ts check_staked_nfts -w EGhbr6W2wV3RA9f4PdTPnJipyN9cZQkLvJhFHoV38how
+# Note: RPC can be updated in .env file
+```
---
+++ cli.ts
@@ -0,0 +1,52 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Command } from "commander";
+import { Coc, IDL } from "./coc";
+
+require("dotenv").config();
+const program = new Command();
+program.version("0.69.420");
+
+const programId = new anchor.web3.PublicKey(
+  "6GGNs4ox9DzjBcNPBypBMW3wFyRSGMX7zomEz4NFKtWv"
+);
+
+const owner = anchor.web3.Keypair.generate();
+
+const connection = new anchor.web3.Connection(
+  process.env.ANCHOR_PROVIDER_URL,
+  "singleGossip"
+);
+
+const walletWrapper = new anchor.Wallet(owner);
+
+const provider = new anchor.Provider(connection, walletWrapper, {
+  preflightCommitment: "recent",
+  skipPreflight: true,
+});
+const anchorProgram = new anchor.Program(
+  IDL,
+  programId,
+  provider
+) as Program<Coc>;
+
+program
+  .command("check_staked_nfts")
+  .description("Get mint list of all the staked NFTs by a wallet")
+  .option("-w, --wallet <string>", "Wallet")
+  .action(async (options, command) => {
+    const pdaList = await anchorProgram.account.stakingAccount.all([
+      {
+        memcmp: {
+          offset: 41, //need to prepend 8 bytes for anchor's disc
+          bytes: options.wallet,
+        },
+      },
+    ]);
+
+    for (const pda of pdaList) {
+      console.log(pda.account.nft.toBase58());
+    }
+  });
+
+program.parse(process.argv);
